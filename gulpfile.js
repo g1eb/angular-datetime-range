98ba3414e96ba3ca12af3e11835ca433619986d7
Gleb <git@gleb.nl>
2016-06-15 11:45:59 +0200

    Added gulp-uglify task to minify javascript that is in dist.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var embedTemplates = require('gulp-angular-embed-templates');
+var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
  
 gulp.task('build', function () {
   gulp.src('src/*.js')
     .pipe(embedTemplates())
+    .pipe(uglify())
     .pipe(gulp.dest('./dist'));
   gulp.src('src/*.css')
     .pipe(cssnano())
