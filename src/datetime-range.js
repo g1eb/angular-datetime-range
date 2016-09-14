fa7379a7f098f65ea604b470bf72cd76c5efb5cf
Gleb <git@gleb.nl>
2016-09-14 15:38:49 +0200

    Added support for jquery events with originalEvent fallback.

--- src/datetime-range.js
+++ src/datetime-range.js
@@ -96,6 +96,7 @@ angular.module('g1b.datetime-range').
         pre: function preLink() {},
         post: function postLink(scope, element, attrs) {
           element.bind('DOMMouseScroll mousewheel wheel', function (ev) {
+            ev = ev.originalEvent || ev;
             var delta = ev.wheelDelta || (-1 * ev.deltaY) || 0;
             if ( delta > 0 ) {
               scope.$apply(function () {
@@ -119,6 +120,7 @@ angular.module('g1b.datetime-range').
         pre: function preLink() {},
         post: function postLink(scope, element, attrs) {
           element.bind('DOMMouseScroll mousewheel wheel', function (ev) {
+            ev = ev.originalEvent || ev;
             var delta = ev.wheelDelta || (-1 * ev.deltaY) || 0;
             if ( delta < 0 ) {
               scope.$apply(function () {
