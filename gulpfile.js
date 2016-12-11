6a86af7436a7ebf95e8ff2d5def1a0796ed5c32c
Gleb <git@gleb.nl>
2016-12-11 15:41:15 +0100

    Bundle minified files in min.js and min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var embedTemplates = require('gulp-angular-embed-templates');
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
  
@@ -7,9 +8,11 @@ gulp.task('build', function () {
   gulp.src('src/*.js')
     .pipe(embedTemplates())
     .pipe(uglify())
+    .pipe(concat('datetime-range.min.js'))
     .pipe(gulp.dest('./dist'));
   gulp.src('src/*.css')
     .pipe(cssnano())
+    .pipe(concat('datetime-range.min.css'))
     .pipe(gulp.dest('./dist'));
 });
 
