6b67bbb8fb051b990be3a748d3a433c32a23ef2a
g1eb <mail@g1eb.com>
2017-11-22 15:10:28 +0100

    Set utc offset on all dates simultaneously and check for 0 offset

--- src/datetime-range.js
+++ src/datetime-range.js
@@ -26,24 +26,22 @@ angular.module('g1b.datetime-range', []).
 
           // Get current date
           scope.current = moment();
-          if ( scope.utcOffset ) {
-            scope.current.utcOffset(scope.utcOffset);
-          }
 
           // Convert start datetime to moment.js if its not a moment object yet
           if ( scope.start && !scope.start._isAMomentObject ) {
             scope.start = moment(scope.start);
-            if ( scope.utcOffset ) {
-              scope.start.utcOffset(scope.utcOffset);
-            }
           }
 
           // Convert end datetime to moment.js if its not a moment object yet
           if ( scope.end && !scope.end._isAMomentObject ) {
             scope.end = moment(scope.end);
-            if ( scope.utcOffset ) {
-              scope.end.utcOffset(scope.utcOffset);
-            }
+          }
+
+          // Set utc offset
+          if ( scope.utcOffset !== undefined ) {
+            scope.current.utcOffset(scope.utcOffset);
+            scope.start.utcOffset(scope.utcOffset);
+            scope.end.utcOffset(scope.utcOffset);
           }
 
           // Get number of weeks in month
