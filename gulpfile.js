4fd3313217825097420ff36ab1ac96a289398885
g1eb <mail@g1eb.com>
2018-03-20 17:26:21 +0100

    Add time-range directive to the build flow in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
 
 gulp.task('build', function () {
-  gulp.src(['src/datetime-range.js', 'src/date-range.js'])
+  gulp.src(['src/datetime-range.js', 'src/date-range.js', 'src/time-range.js'])
     .pipe(embedTemplates())
     .pipe(uglify())
     .pipe(concat('datetime-range.min.js'))
     .pipe(gulp.dest('./dist'));
-  gulp.src(['src/datetime-range.css', 'src/date-range.css'])
+  gulp.src(['src/datetime-range.css', 'src/date-range.css', 'src/time-range.css'])
     .pipe(cssnano())
     .pipe(concat('datetime-range.min.css'))
     .pipe(gulp.dest('./dist'));
